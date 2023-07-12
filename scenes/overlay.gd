94bf1fcfebbb1adb0ab18a99ac63f78e5ce296af
Falco90 <falcorodenburg@gmail.com>
2023-07-12 12:10:13 +0200

    added comments to overlay file

--- scenes/overlay.gd
+++ scenes/overlay.gd
@@ -1,11 +1,12 @@
 extends CanvasLayer
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	for character in PlayerStatus.get_characters():
 		update_stats(character)
 		populate_inventory(character)
 
+#Synchronizes the UI with the latest state of the Charcter Dictionary
 func update_stats(num):
 	var characterNode = get_node("Character" + str(num))
 	var character = PlayerStatus.get_character(num)
@@ -33,7 +34,8 @@ func update_stats(num):
 
 	characterNode.get_node("Gold").get_node("Value").text = "Gold: " + str(character.gold)
 	characterNode.get_node("Kills").get_node("Value").text = "Monster Kills: " + str(character.kills)
-	
+
+#Synchronizes the Inventory UI with the latest state of the Inventory Resource in the Character Dictionary
 func populate_inventory(num):
 	var characterNode = get_node("Character" + str(num))
 	var inventory = characterNode.get_node("InventoryPanel").get_node("Inventory")
@@ -42,7 +44,8 @@ func populate_inventory(num):
 	
 	inventory.set_inventory_data(character.inventory)
 	equipped_items.set_inventory_data(character.equipped_items)
-	
+
+#Toggle Overlay
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("inventory"):
 		self.visible = !self.visible
